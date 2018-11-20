0aecf0cd6b25cea465f819fa2610f6ec024fd778
Guille <juarez.unlp96@gmail.com>
2018-11-20 10:55:26 -0300

    prueba

--- Api.php
+++ Api.php
@@ -5,6 +5,6 @@ use GuzzleHttp\Client;
 
 $client = new Client(['base_uri' => 'https://api-referencias.proyecto2018.linti.unlp.edu.ar/', 'timeout'  => 2.0]);
 
-$response = $client->request('GET', 'obra-social/2');
+$response = $client->request('GET', 'obra-social');
 
 echo $response->getBody();
