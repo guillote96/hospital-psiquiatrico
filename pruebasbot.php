063961863945d1728e72930eaed090d0b8a14cea
Guille <juarez.unlp96@gmail.com>
2018-11-23 18:33:35 -0300

    api con slim

--- pruebasbot.php
+++ pruebasbot.php
@@ -46,7 +46,12 @@ switch ($cmd) {
 
  
     case '/instituciones':
-        $msg['text']  = file_get_contents("https://grupo2.proyecto2018.linti.unlp.edu.ar/api/index.php/instituciones/".$cmd_params[0],false);
+       $params = explode(" ", $cmd_params);
+       if(count($params) > 0){
+        $msg['text']  = file_get_contents("https://grupo2.proyecto2018.linti.unlp.edu.ar/api/index.php/instituciones/".$params[0],false);
+        }else{
+          $msg['text']  = file_get_contents("https://grupo2.proyecto2018.linti.unlp.edu.ar/api/index.php/instituciones",false);
+        }
         break;
  
     default:
