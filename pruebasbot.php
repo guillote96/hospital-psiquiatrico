585be55eb78a05ec5aee0ebf0b6e560f7cd41e93
Nicolás Melones <nicolasmelones@live.com.ar>
2018-11-25 20:52:58 -0300

    prueba

--- pruebasbot.php
+++ pruebasbot.php
@@ -41,7 +41,7 @@ switch ($cmd) {
         //$msg['text'] .= '/start Inicializa el bot' . PHP_EOL;
         $msg['text'] .= '/instituciones Lista las instituciones' . PHP_EOL;
         $msg['text'] .= '/instituciones-region-sanitaria:{id}' . PHP_EOL;
-        $msg['text'] .= '/help Muestra esta ayuda1';
+        $msg['text'] .= '/help Muestra esta ayuda';
         $msg['reply_to_message_id'] = null;
         break;
 
@@ -49,8 +49,7 @@ switch ($cmd) {
     case '/instituciones':
         $informacion = file_get_contents("https://grupo2.proyecto2018.linti.unlp.edu.ar/api/index.php/instituciones",false);
         $informacion = json_decode($informacion,true);
-        echo $informacion->nombre;
-        $msg['text']  = $informacion->nombre; //file_get_contents("https://grupo2.proyecto2018.linti.unlp.edu.ar/api/index.php/instituciones",false);
+        $msg['text']  = $informacion['nombre']; //file_get_contents("https://grupo2.proyecto2018.linti.unlp.edu.ar/api/index.php/instituciones",false);
         break;
 
     case '/instituciones-region-sanitaria':
