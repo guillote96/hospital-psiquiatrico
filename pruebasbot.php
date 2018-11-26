966cd011195e9c0f4ccac0f1c4502945a060ab57
Nicolás Melones <nicolasmelones@live.com.ar>
2018-11-25 21:11:37 -0300

    prueba

--- pruebasbot.php
+++ pruebasbot.php
@@ -41,15 +41,15 @@ switch ($cmd) {
         //$msg['text'] .= '/start Inicializa el bot' . PHP_EOL;
         $msg['text'] .= '/instituciones Lista las instituciones' . PHP_EOL;
         $msg['text'] .= '/instituciones-region-sanitaria:{id}' . PHP_EOL;
-        $msg['text'] .= '/help Muestra esta ayuda';
+        $msg['text'] .= '/help Muestra esta ayuda1';
         $msg['reply_to_message_id'] = null;
         break;
 
  
     case '/instituciones':
-        //$informacion = file_get_contents("https://grupo2.proyecto2018.linti.unlp.edu.ar/api/index.php/instituciones",false);
+        $informacion = '{"id":"1","nombre":"Hospital Municipal de Rivadavia","director":"Dr.Vergara","telefono":"20392","region_sanitaria_id":"1","tipo_institucion_id":"1"},{"id":"2","nombre":"Clinica los tilos","director":"nose","telefono":"223","region_sanitaria_id":"1","tipo_institucion_id":"1"},{"id":"3","nombre":"Hospital 1","director":"Juan Perez","telefono":"1234","region_sanitaria_id":"1","tipo_institucion_id":"1"}';
         //$informacion = json_decode($informacion,true);
-        $msg['text']  = '{"id":"1","nombre":"Hospital Municipal de Rivadavia","director":"Dr.Vergara","telefono":"20392","region_sanitaria_id":"1","tipo_institucion_id":"1"},{"id":"2","nombre":"Clinica los tilos","director":"nose","telefono":"223","region_sanitaria_id":"1","tipo_institucion_id":"1"},{"id":"3","nombre":"Hospital 1","director":"Juan Perez","telefono":"1234","region_sanitaria_id":"1","tipo_institucion_id":"1"}'; //file_get_contents("https://grupo2.proyecto2018.linti.unlp.edu.ar/api/index.php/instituciones",false);
+        $msg['text']  = json_decode($informacion); //file_get_contents("https://grupo2.proyecto2018.linti.unlp.edu.ar/api/index.php/instituciones",false);
         break;
 
     case '/instituciones-region-sanitaria':
